b7cfb51d8ccad5b43931c81d0642798f075229d6
AnMado <qjrpcg@gmail.com>
2022-05-13 15:38:05 +0300

    Init

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,15 @@
+import java.io.FileReader
+
+fun main(args: Array<String>){
+    val fileReader = FileReader("./input/Input.doc")
+    val arrayData = fileReader.readLines()
+    val arraySize = arrayData.size
+
+    var arrayModified = DoubleArray(arraySize)
+
+    for (number in arrayData){
+
+    }
+
+
+}
