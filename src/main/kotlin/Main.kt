06e225177eb59c317c83631c014f0df6ab0a0804
AnMado <qjrpcg@gmail.com>
2022-05-13 16:06:26 +0300

    Algorithm is ready. Only output to do

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,15 +1,27 @@
 import java.io.FileReader
+import kotlin.math.sign
 
 fun main(args: Array<String>){
     val fileReader = FileReader("./input/Input.doc")
-    val arrayData = fileReader.readLines()
-    val arraySize = arrayData.size
+    val array = fileReader.readLines()
+    val arraySize = array.size
 
     var arrayModified = DoubleArray(arraySize)
 
-    for (number in arrayData){
+    var indexBegin = 0
+    var indexEnd = arraySize - 1
 
+    for (line in array){
+        var number = line.toDouble()
+        if (number.sign < 0.0) {
+            arrayModified[indexBegin] = number
+            indexBegin += 1
+        } else {
+            arrayModified[indexEnd] = number
+            indexEnd -= 1
+        }
     }
+
 
 
 }
